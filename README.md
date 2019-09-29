e17cffc725236a5545649395a6704e4e78f6a760
ailtonbsj <ailton.ifce@gmail.com>
2019-09-29 01:37:31 -0300

    added readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# irydium-chemistry-lab-bionic
+
+> This is a fork from IrYdium Project's Team. It is available in:
+> [http://chemcollective.org/](http://chemcollective.org/vlab_download)
+
+An Virtual Chemistry Laboratory from The irYdium Project's.
+
+You need Java installed to run correctly.
+
+Versão: 2.1.0
+
+[Baixe o pacote Debian aqui!](https://drive.google.com/open?id=1eCZvCs_j_DpyYMUbPtecPvo9RfUNJ23F)
+
+## Screeenshot
+
+![](screenshot.png?raw=true)
--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# if [ ! -f deb_package/usr/bin/irydium-chemistry-lab ]; then
-# 	wget "" -O irydium-chemistry-lab.tar.xz
-# 	tar -xJf irydium-chemistry-lab.tar.xz
-# 	rm -rf deb_package/usr/share/irydium/
-# 	mv irydium/ deb_package/usr/share/
-# fi
+if [ ! -d deb_package/usr/share/irydium/ ]; then
+	wget "https://drive.google.com/uc?id=1hkkzWb8Sa_Gnw7c4PA10qOna-nZt97kn&export=download" -O irydium-chemistry-lab.tar.xz
+	tar -xJf irydium-chemistry-lab.tar.xz
+	rm -rf deb_package/usr/share/irydium/
+	mv irydium/ deb_package/usr/share/
+fi
 
 dpkg-deb -b deb_package .
